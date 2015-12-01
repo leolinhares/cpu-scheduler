e62ac2d0871d9bf4ca617b6cca5b7a6067aa50f6
brunohensc <brunohenrique@mail.com>
2015-12-01 19:28:32 -0200

    First implementation of Writer Class
    Need improving
    :dancers:

---
+++ src/Writer.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * Created by Bruno on 01/12/2015.
+ */
+public class Writer {
+    public static  void  main(String[] args){
+        BufferedWriter escrever = null;
+        try{
+            File arquivoDeLog = new File();// passar os valores para criar o arquivo
+            escrever = new BufferedWriter( new FileWriter()); // escrever no arquivo os valores passados
+            escrever.write("qualquer coisa");
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            try{
+                escrever.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
