a56e02e4635e4f168edf274c362800c4b2a86a6e
Leo Linhares <slinharesleo@gmail.com>
2015-12-01 23:09:14 -0300

    rr class added

---
+++ src/RR.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Created by leolinhares on 01/12/15.
+ */
+public class RR extends Scheduler {
+
+    public RR(List<Process> processes) {
+        super(processes);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
